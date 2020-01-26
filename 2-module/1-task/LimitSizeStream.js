b2e97043e81c3b8deb1abb0b1077efe8d269304f
azh-dev <azhdevelopment@gmail.com>
2020-01-26 06:37:08 +0300

    add solution for 2-module 1-task 2-task

--- 2-module/1-task/LimitSizeStream.js
+++ 2-module/1-task/LimitSizeStream.js
@@ -4,10 +4,20 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+
+    this.limit = options.limit;
+    this.bytesTransferred = 0;
   }
 
   _transform(chunk, encoding, callback) {
+    this.bytesTransferred += chunk.length;
 
+    if (this.bytesTransferred <= this.limit) {
+      this.push(chunk.toString('utf-8'));
+      callback();
+    } else {
+      callback(new LimitExceededError());
+    }
   }
 }
 
--- 2-module/2-task/LineSplitStream.js
+++ 2-module/2-task/LineSplitStream.js
@@ -4,12 +4,40 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+
+    this.buffer = [];
   }
 
   _transform(chunk, encoding, callback) {
+    const _separator = os.EOL;
+    const _chunk = chunk.toString('utf-8');
+
+    this.buffer.push(_chunk);
+
+    if (_chunk.indexOf(_separator) === -1) {
+      callback();
+      return;
+    }
+
+    const bufferValue = [].concat(this.buffer).join('');
+    const chunkList = bufferValue.split(_separator);
+
+    while (chunkList.length > 1) {
+      this.push(chunkList[0]);
+      chunkList.shift();
+    }
+
+    this.buffer = chunkList;
+
+    callback();
   }
 
   _flush(callback) {
+    if (this.buffer.length > 0) {
+      this.push([].concat(this.buffer).join(''));
+    }
+
+    callback();
   }
 }
 
