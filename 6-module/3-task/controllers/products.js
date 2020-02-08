4006470cb4e4c2df2b4ab004f5e96c5d5cfe1732
admin <admin@admin.local>
2020-02-08 23:25:47 +0300

    add solution 6-module 3-task

--- 6-module/3-task/controllers/products.js
+++ 6-module/3-task/controllers/products.js
@@ -1,11 +1,21 @@
 const Product = require('../models/Product');
 
+function productMapper(product) {
+  return {
+    id: product.id,
+    title: product.title,
+    images: product.images,
+    category: product.category,
+    subcategory: product.subcategory,
+    price: product.price,
+    description: product.description,
+  };
+}
+
 module.exports.productsByQuery = async function productsByQuery(ctx, next) {
   const query = ctx.query.query;
-
-  const products = await Product.find({$text: {$search: query}});
-
-  console.log(query, products);
+  let products = await Product.find({$text: {$search: query}});
+  products = products.map(productMapper);
 
   ctx.body = {products: products};
 };
