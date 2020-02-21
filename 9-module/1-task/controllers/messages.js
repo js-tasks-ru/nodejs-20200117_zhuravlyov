04b3b22b043832bfcc4644fb3f96365c92f864fc
azh-dev <azhdevelopment@gmail.com>
2020-02-21 03:22:38 +0300

    add solution 9-module 1-task

--- 9-module/1-task/controllers/messages.js
+++ 9-module/1-task/controllers/messages.js
@@ -1,5 +1,19 @@
 const Message = require('../models/Message');
 
 module.exports.messageList = async function messages(ctx, next) {
-  ctx.body = {messages: []};
+  let messages = await Message.find({chat: ctx.user._id}, null, {limit: 20});
+  messages = messages.map(mapMessage);
+
+  ctx.body = {
+    messages,
+  };
 };
+
+function mapMessage(message) {
+  return {
+    date: message.date.toISOString(),
+    text: message.text,
+    id: message._id,
+    user: message.user,
+  };
+}
--- 9-module/1-task/socket.js
+++ 9-module/1-task/socket.js
@@ -6,12 +6,34 @@ const Message = require('./models/Message');
 function socket(server) {
   const io = socketIO(server);
 
-  io.use(async function(socket, next) {
+  io.use(async (socket, next) => {
+    const token = socket.handshake.query.token;
+
+    socket.session = await Session.findOne({
+      token: token,
+    }).populate('user');
+
     next();
   });
 
-  io.on('connection', function(socket) {
-    socket.on('message', async (msg) => {});
+  io.on('connection', (socket) => {
+    if (!socket.session) {
+      io.emit('error', 'anonymous sessions are not allowed');
+      io.emit('close');
+    }
+
+    socket.on('message', async (msg) => {
+      await Message.create({
+        date: new Date(),
+        text: msg,
+        chat: socket.session.user._id,
+        user: socket.session.user.displayName,
+      });
+    });
+
+    io.on('error', (err) => {
+      console.log(err);
+    });
   });
 
   return io;
